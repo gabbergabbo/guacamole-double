c77c921b10b21b7cbdb86f96850f3b2504995071
unknown <gabbodb@gmail.com>
2020-06-02 23:54:40 +0200

    cleanup

---
+++ Source/Platformer2D/PlayerAnimationState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/UserDefinedEnum.h"
+#include "PlayerAnimationState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMER2D_API UPlayerAnimationState : public UUserDefinedEnum
+{
+	GENERATED_BODY()
+	
+};
